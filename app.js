04bdd5a04a50504938394fed025e460935b1d0e9
Offiahchiaz <offiahchiaz@gmail.com>
2019-01-02 15:51:40 +0100

    Add GET and POST /users/signup

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express(); 
 const port = process.env.PORT || 5000;
@@ -20,6 +22,23 @@ app.set('view engine', 'ejs');
 
 // Bodyparser
 app.use(express.urlencoded({ extended: false }));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Connect flash
+app.use(flash()); 
+
+// Global vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
 
 // ROUTES
 app.use('/', require('./routes/index'));
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,15 @@
 const express = require('express');
-
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+// User model
+const User = require('../models/user');
 
 // Login Page
-router.get('/login', (req, res) => {
-    res.render('login');
-});
+router.get('/login', (req, res) =>  res.render('login'));
 
 // Signup Page
-router.get('/signup', (req, res) => {
-    res.render('signup');
-});
+router.get('/signup', (req, res) => res.render('signup'));
 
 // Signup POST
 router.post('/signup', (req, res) => {
@@ -33,9 +32,41 @@ router.post('/signup', (req, res) => {
     }
 
     if (errors.length > 0) {
-        res.render('signup', { errors, name, email, password, password2});
+        res.render('signup', { errors, name, email, password, password2 });
     } else {
- 
+        // Validation passed
+        User.findOne({email: email})
+            .then((user) => {
+                if (user) {
+                    // User exists
+                    errors.push({msg: 'Email is already registered'});
+                    res.render('signup', { errors, name, email, password, password2 });
+                } else {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password
+                    });
+
+                    // Hash password
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) throw err;
+                            // Set password to hashed
+                            newUser.password = hash;
+                            // Save user
+                            newUser.save()
+                                .then((user) => {
+                                    console.log(user);
+                                    req.flash('success_msg', 'you are now registered');
+                                    res.redirect('/users/login');
+                                })
+                                .catch((e) => console.log(e));
+                        });
+                    });
+                }
+            });
+
     }
 });
 
