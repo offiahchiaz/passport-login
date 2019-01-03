c28269500f8fa58bb287cb9d8edc0de26d04c85d
Offiahchiaz <offiahchiaz@gmail.com>
2019-01-03 01:34:58 +0100

    Disable caching for restricted page

--- app.js
+++ app.js
@@ -49,6 +49,13 @@ app.use((req, res, next) => {
     next();
 });
 
+// caching disabled for every route
+app.use(function(req, res, next) {
+    res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+    next();
+});
+  
+
 // ROUTES
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
