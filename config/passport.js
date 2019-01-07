568212b0b646199ef494bc0b6e7ec0d5e2d73c0b
Offiahchiaz <offiahchiaz@gmail.com>
2019-01-07 23:38:58 +0100

    Minor changes

--- config/passport.js
+++ config/passport.js
@@ -12,14 +12,14 @@ module.exports = function (passport) {
             User.findOne({email: email})
                 .then((user) => {
                     if (!user) {
-                        return done(null, false, {message: 'That email is not registered'})
+                        return done(null, false, {message: 'That email is not registered'});
                     }
 
                     // Match password
                     bcrypt.compare(password, user.password, (err, isMatch) => {
                         if (err) throw err;
 
-                        if(isMatch) {
+                        if (isMatch) {
                             return done(null, user);
                         } else {
                             return done(null, false, {message: 'Password incorrect'});
